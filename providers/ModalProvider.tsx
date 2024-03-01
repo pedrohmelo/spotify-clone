644ff959f246e130732573a7116ba88a0c5c829f
pedrohmelo <phmc_1995@hotmail.com>
2024-03-01 09:48:10 -0300

    feat: added radix-ui packages

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Modal from "@/components/Modal";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  //to avoid modals being rendered on server side, lets use this useEffect
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <Modal />
+    </>
+  );
+};
+
+export default ModalProvider;
