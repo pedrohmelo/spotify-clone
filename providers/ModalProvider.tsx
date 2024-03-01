55ec72e2a3b6888b53dd3184c25fb66b46d80594
pedrohmelo <phmc_1995@hotmail.com>
2024-03-01 10:48:44 -0300

    feat: finished modal and added zustand packages

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -18,7 +18,14 @@ const ModalProvider = () => {
 
   return (
     <>
-      <Modal />
+      <Modal
+        title="Test Modal"
+        description="Test Description"
+        isOpen
+        onChange={() => {}}
+      >
+        Test Children
+      </Modal>
     </>
   );
 };
